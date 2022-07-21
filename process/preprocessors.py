7f3d3d3b6b8221a6689bf441e9bf8b169324eab0
ahayek84 <ahayek84@gmail.com>
2022-07-21 23:12:50 +0200

    Reading files

--- process/preprocessors.py
+++ process/preprocessors.py
@@ -3,3 +3,17 @@
 ABI
 1 - Data loading and imputation 
 """
+import pandas as pd
+
+def read_data():
+    files = ["mongodb","react","socketio"]
+    frames = []
+    for file in files:
+        df = pd.read_csv(f"./data/{file}.csv",sep=";",encoding="utf-8")
+        df["source"] = [f"{file}" for _ in range(len(df))]
+        frames.append(df)
+    return pd.concat(frames)
+
+if __name__ == "__main__":
+    data = read_data()
+    print(data.info())

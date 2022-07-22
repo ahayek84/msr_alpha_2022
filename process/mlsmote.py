6e28b4cdf286a2206095bc7dfd1aaef84ccba722
abdullrahman yusuf elhayek <ahayek84@gmail.com>
2022-07-22 11:26:17 +0200

    Update mlsmote.py
    pip install smote-variants
    
    --- 
    
    just install the smote-variants

--- process/mlsmote.py
+++ process/mlsmote.py
@@ -21,121 +21,27 @@ def create_dataset(n_sample=1000):
     X, y = make_classification(n_classes=5, class_sep=2, 
                            weights=[0.1,0.025, 0.205, 0.008, 0.9], n_informative=3, n_redundant=1, flip_y=0,
                            n_features=10, n_clusters_per_class=1, n_samples=1000, random_state=10)
-    y = pd.get_dummies(y, prefix='class')
+    #print(y)
+    #y = pd.get_dummies(y, prefix='class')
     return pd.DataFrame(X), y
 
-def get_tail_label(df):
-    """
-    Give tail label colums of the given target dataframe
-    
-    args
-    df: pandas.DataFrame, target label df whose tail label has to identified
-    
-    return
-    tail_label: list, a list containing column name of all the tail label
-    """
-    columns = df.columns
-
-    n = len(columns)
-    irpl = np.zeros(n)
-    for column in range(n):
-        irpl[column] = df[columns[column]].value_counts()[1]
-        
-    print(irpl)
-    irpl = max(irpl)/irpl
-    print(" ss ", irpl)
-
-    mir = np.average(irpl)
-    tail_label = []
-    for i in range(n):
-        if irpl[i] > mir:
-            tail_label.append(columns[i])
-    return tail_label
-
-def get_index(df):
-  """
-  give the index of all tail_label rows
-  args
-  df: pandas.DataFrame, target label df from which index for tail label has to identified
-    
-  return
-  index: list, a list containing index number of all the tail label
-  """
-  tail_labels = get_tail_label(df)
-  index = set()
-  for tail_label in tail_labels:
-    sub_index = set(df[df[tail_label]==1].index)
-    index = index.union(sub_index)
-  return list(index)
-
-def get_minority_instace(X, y):
-    """
-    Give minority dataframe containing all the tail labels
-    
-    args
-    X: pandas.DataFrame, the feature vector dataframe
-    y: pandas.DataFrame, the target vector dataframe
-    
-    return
-    X_sub: pandas.DataFrame, the feature vector minority dataframe
-    y_sub: pandas.DataFrame, the target vector minority dataframe
-    """
-    index = get_index(y)
-    X_sub = X[X.index.isin(index)].reset_index(drop = True)
-    y_sub = y[y.index.isin(index)].reset_index(drop = True)
-    return X_sub, y_sub
-
-def nearest_neighbour(X):
-    """
-    Give index of 5 nearest neighbor of all the instance
-    
-    args
-    X: np.array, array whose nearest neighbor has to find
-    
-    return
-    indices: list of list, index of 5 NN of each element in X
-    """
-    nbs=NearestNeighbors(n_neighbors=5,metric='euclidean',algorithm='kd_tree').fit(X)
-    euclidean,indices= nbs.kneighbors(X)
-    return indices
-
-def MLSMOTE(X,y, n_sample):
-    """
-    Give the augmented data using MLSMOTE algorithm
-    
-    args
-    X: pandas.DataFrame, input vector DataFrame
-    y: pandas.DataFrame, feature vector dataframe
-    n_sample: int, number of newly generated sample
-    
-    return
-    new_X: pandas.DataFrame, augmented feature vector data
-    target: pandas.DataFrame, augmented target vector data
-    """
-    indices2 = nearest_neighbour(X)
-    n = len(indices2)
-    new_X = np.zeros((n_sample, X.shape[1]))
-    target = np.zeros((n_sample, y.shape[1]))
-    for i in range(n_sample):
-        reference = random.randint(0,n-1)
-        neighbour = random.choice(indices2[reference,1:])
-        all_point = indices2[reference]
-        nn_df = y[y.index.isin(all_point)]
-        ser = nn_df.sum(axis = 0, skipna = True)
-        target[i] = np.array([1 if val>2 else 0 for val in ser])
-        ratio = random.random()
-        gap = X.loc[reference,:] - X.loc[neighbour,:]
-        new_X[i] = np.array(X.loc[reference,:] + ratio * gap)
-    new_X = pd.DataFrame(new_X, columns=X.columns)
-    target = pd.DataFrame(target, columns=y.columns)
-    new_X = pd.concat([X, new_X], axis=0)
-    target = pd.concat([y, target], axis=0)
-    return new_X, target
+def MLSMOTE(X_train, y_train,synthetic_balance_proportion):
+    import smote_variants as svs
+    cols = X_train.columns
+    oversampler = svs.SMOTE(proportion=synthetic_balance_proportion)
+    oversampler = svs.MulticlassOversampling(oversampler)
+    X_train, y_train = oversampler.sample(X_train.values, y_train)
+    X_train_res = pd.DataFrame(columns=cols)
+    i = 0
+    for col in cols:
+        X_train_res[col] = X_train[:,i] ##### llsls [:,2]
+        i = i + 1
+    return X_train_res, y_train
 
 if __name__=='main':
     """
     main function to use the MLSMOTE
     """
     X, y = create_dataset()                     #Creating a Dataframe
-    X_sub, y_sub = get_minority_instace(X, y)   #Getting minority instance of that datframe
-    X_res,y_res =MLSMOTE(X_sub, y_sub, 100)     #Applying MLSMOTE to augment the dataframe
+    X_res,y_res =MLSMOTE(X, y, 0.12)     #Applying MLSMOTE to augment the dataframe
+    print(X.shape,len(y),X_res.shape,len(y_res))

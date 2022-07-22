e68cb9c33d7d6e936d409b5b86ceed66d03c530e
ahayek84 <ahayek84@gmail.com>
2022-07-22 20:04:14 +0200

    ssss

--- process/preprocessors.py
+++ process/preprocessors.py
@@ -17,15 +17,19 @@ def read_data():
         frames.append(df)
     return pd.concat(frames)
 
-def data_preprocessing():
+def data_preprocessing(source="all"):
     data = read_data()
+
+    if source in ["mongodb","react","socketio"]:
+        data = data.loc[data.source == source,:]
+
     cols = ["login","name","email","source"]
     res = MultiColumnLabelEncoder(columns = cols).fit_transform(data)
     res = res.fillna(0)
     return res
 
-def create_dataset():
-    all_data = data_preprocessing()
+def create_dataset(source="all"):
+    all_data = data_preprocessing(source=source)
     all_data.to_excel(f"./data/all_data.xlsx")
     X = all_data.loc[:,all_data.columns != "survey"]
     y = [x[0] for x in all_data[["survey"]].values]
@@ -34,6 +38,6 @@ def create_dataset():
 
 
 if __name__ == "__main__":
-    X, y = create_dataset()  
+    X, y = create_dataset(source="react")  
     print(X.shape)
     #X.to_excel(f"./data/all_X_data.xlsx")
